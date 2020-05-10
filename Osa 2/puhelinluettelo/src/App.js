3dc436b8cebab820712c042c1f9771050f29c189
alluvm <aleksi.jalmanen@gmail.com>
2020-05-10 17:02:06 +0300

    2.11 done

--- Osa 2/puhelinluettelo/src/App.js
+++ Osa 2/puhelinluettelo/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
+import axios from "axios"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState("")
   const [ newFilter, setNewFilter ] = useState("")
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data)
+    })
+  }
+
+  useEffect(hook,[])
 
 
   const handleClick = (event) => {
@@ -51,7 +55,6 @@ const App = () => {
         <PersonForm handleName = {handleName} handleClick= {handleClick} handlePhone={handlePhone}></PersonForm>
       <h2>Numbers</h2>
         <Persons people={persons} filter={newFilter}></Persons>
-
     </div>
   )
 
